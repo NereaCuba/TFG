1639e9739dddecd666440206e3547cf999d8db35
Nono <nono-loja@hotmail.com>
2023-03-20 17:31:24 +0100

    Added evaluate form

--- src/app/modules/evaluate/evaluate.module.ts
+++ src/app/modules/evaluate/evaluate.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { EvaluateComponent } from './evaluate/evaluate.component';
 import { RouterModule } from '@angular/router';
 import { evaluateRoutingModule } from './evaluate-routing.module';
-
+import {RadioButtonModule} from 'primeng/radiobutton';
 import {TooltipModule} from 'primeng/tooltip';
+import { FormsModule } from '@angular/forms';
+import { UbayButtonModule } from 'app/components/ubay-button/ubay-button.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,8 +16,12 @@ import {TooltipModule} from 'primeng/tooltip';
   ],
   imports: [
     CommonModule,
+    RadioButtonModule,
     TooltipModule,
-    evaluateRoutingModule
+    evaluateRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    UbayButtonModule
   ],
   providers: [
     RouterModule
--- src/app/modules/evaluate/evaluate/evaluate.component.ts
+++ src/app/modules/evaluate/evaluate/evaluate.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,9 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./evaluate.component.scss']
 })
 export class EvaluateComponent {
-
+  typeImport: string = '';
+  nameFile: string = 'No se ha seleccionado ningún archivo';
+  file:any = null; 
+  dbImage: any = null;
+  postResponse: any = null;
+  successResponse: string = null;
+  image: any = null;
   constructor(    
     private router: Router,
+    private httpClient: HttpClient
   ) {}
   redirectTo(uri: string) {
     this.router.navigateByUrl('/', {skipLocationChange: true}).then(() =>
@@ -17,5 +25,38 @@ export class EvaluateComponent {
   }
   redirectToHeuristics() {
     this.redirectTo('/heuristics-list');
+  }
+  manageImage(event) {
+    this.file = event.target.files[0];
+    this.nameFile = this.file.name;
+    this.imageUploadAction();
+    this.viewImage();
+  }
+
+  imageUploadAction() {
+    const imageFormData = new FormData();
+    imageFormData.append('image', this.file, this.file.name);
+
+
+    this.httpClient.post('http://localhost:4200/upload/image/', imageFormData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.postResponse = response;
+          this.successResponse = this.postResponse.body.message;
+        } else {
+          this.successResponse = 'Image not uploaded due to some error!';
+        }
+      }
+      );
+    }
+
+  viewImage() {
+    this.httpClient.get('http://localhost:4200/get/image/info/' + this.image)
+      .subscribe(
+        res => {
+          this.postResponse = res;
+          this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+        }
+      );
   }
 }
